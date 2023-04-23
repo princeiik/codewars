ec173b7e55dc063dd57f9ef141c2348e159948fc
Prince Kanu <kanuprince93@yahoo.com>
2023-04-23 02:06:04 -0500

    added codewars challenges

---
+++ 8kyu/grasshopper-check-for-factor.js
@@ -0,0 +1,10 @@
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+//My Solutions
+
+function checkForFactor (base, factor) {
+    return base%factor === 0
+  }
+  
