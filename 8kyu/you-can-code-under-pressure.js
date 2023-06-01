008ba8e0789a942c51e10d62639d64bd82c972fe
Prince Kanu <kanuprince93@yahoo.com>
2023-05-31 22:17:24 -0500

    added codewars challenge

---
+++ 8kyu/you-can-code-under-pressure.js
@@ -0,0 +1,9 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+//Solution
+//P: an integer of any kind
+//R: double the integer
+
+function doubleInteger(i) {
+  return i * 2;
+}
