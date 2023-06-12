2dba3059d0c8e9fbee79900521325b7c113325dc
Prince Kanu <kanuprince93@yahoo.com>
2023-06-11 20:37:08 -0500

    added codewars challenge

---
+++ 8kyu/function-2-squaring-an-argument.js
@@ -0,0 +1,12 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//Solution
+//P: 3 inputs, name, city and state. Name will be an array with one or more values and will vary
+//R: A welcome message to the person. The name array will have a space between first and last name
+
+function square(num) {
+  //return the square of the number
+  return num * num
+}
+
+console.log(sqaureofNum(3)); //9
