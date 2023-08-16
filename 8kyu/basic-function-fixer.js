96c57f68363f3a328cdb57ebe0f10b436174f4d4
Prince Kanu <kanuprince93@yahoo.com>
2023-08-16 17:53:49 -0500

    added codewars challenge

---
+++ 8kyu/basic-function-fixer.js
@@ -0,0 +1,15 @@
+// Fix the function
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+function addFive(num) {
+  var total = num + 5
+  //change nums to total so it shows total
+  return total
+}
+
+
+console.log(addFive(5)); //10
+console.log(addFive(0)); //5
+console.log(addFive(-5)); //0
