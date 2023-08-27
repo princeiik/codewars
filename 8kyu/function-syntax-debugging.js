fe9029c09b704fe8b24d72bc9efdcec1045969f6
Prince Kanu <kanuprince93@yahoo.com>
2023-08-26 20:18:48 -0500

    added codewars challenge

---
+++ 8kyu/function-syntax-debugging.js
@@ -0,0 +1,12 @@
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+//Solution
+//P: 2 parameter containing a verb and noun
+//R: the mistake fixed so the code makes sense
+
+function main (verb, noun) {
+  return `${verb}${noun}`
+}
+
+console.log(main ('take ', 'item')); //'take item'
+console.log(main ('use ', 'sword')); //'use sword'
