a22220fa2e46df49c977c2ba64218b43115bb9b7
Prince Kanu <kanuprince93@yahoo.com>
2023-09-06 10:37:47 -0500

    added codewars challenge

---
+++ 8kyu/find-the-remainder.js
@@ -0,0 +1,32 @@
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Division by zero should return NaN.
+
+// Examples:
+// n = 17
+// m = 5
+// result = 2 (remainder of `17 / 5`)
+
+// n = 13
+// m = 72
+// result = 7 (remainder of `72 / 13`)
+
+// n = 0
+// m = -1
+// result = 0 (remainder of `0 / -1`)
+
+// n = 0
+// m = 1
+// result - division by zero (refer to the specifications on how to handle this in your language)
+
+//Solution
+//P: two numbers. 
+//R: a number that is the remainder of the two numbers. Divid the larger by the smaller value
+
+function remainder(n, m){
+  //Get remainder of the larger number by the smaller number using modulus
+  return n > m ? n % m : m % n  
+}
+
+console.log(remainder(17,5)); //2
+console.log(remainder(1, 0)); //NaN
